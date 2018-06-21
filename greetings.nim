ff3762f10f9bd01b8f227bdbdd9e4d328f904cb9
Dan Harper <dan@kingdomsolutions.com.au>
2018-06-21 23:52:23 +1000

    Now with case statements!

--- greetings.nim
+++ greetings.nim
@@ -1,9 +1,12 @@
 # This is a comment
 echo "What is your name? "
 let name: string = readLine(stdin)
-if name == "":
+case name
+of "":
   echo "Oh you poor soul, you've lost your name!"
-elif name == "name":
+of "name":
   echo "Haha, very funny, your name is name!"
+of "Dan", "Daniel":
+  echo "Awesome name ", name, "!"
 else:
   echo "Hi, ", name, "!"
